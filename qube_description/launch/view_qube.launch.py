2cea57035b868432ab5c4c476d132dd8bd1bd990
Vegardbp <103881675+Vegardbp@users.noreply.github.com>
2025-03-24 16:10:10 +0100

    rviz and launch donezo

--- qube_description/launch/view_qube.launch.py
+++ qube_description/launch/view_qube.launch.py
@@ -4,8 +4,10 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+package_name = 'qube_description'
+
 def generate_launch_description():
-    xacro_file = os.path.join(get_package_share_directory("qube_description"),"urdf","qube.urdf.xacro")
+    xacro_file = os.path.join(get_package_share_directory(package_name),"urdf","qube.urdf.xacro")
     robot_description_content = xacro.process_file(xacro_file).toxml()   
 
     node_robot_state_publisher = Node(
@@ -17,7 +19,7 @@ def generate_launch_description():
 
     # RViz
     rviz_config = os.path.join(
-        get_package_share_directory('qube_bringup'),
+        get_package_share_directory(package_name),
         'config',
         'rviz_config.rviz'
     )
--- qube_description/setup.py
+++ qube_description/setup.py
@@ -1,5 +1,8 @@
 from setuptools import find_packages, setup
 
+import os
+from glob import glob
+
 package_name = 'qube_description'
 
 setup(
@@ -10,6 +13,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.xacro*')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.rviz')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
